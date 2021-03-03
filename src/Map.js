e91e138bcd7ed5cf7519fde9cc21cf394a2d6d04
ssmitchell <stephenmitchell2299@gmail.com>
2021-03-03 16:36:28 +0000

    add support for dummy django backend

--- src/Map.js
+++ src/Map.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import mapboxgl from 'mapbox-gl';
 import map_data from './test.geojson';
+import axios from 'axios';
+import backend from './globals'
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiaXNhYWNqbWlsbGVyIiwiYSI6ImNrMTZ6NnBqdjFiM3czcHRrb3ZtbTZsajYifQ.3tv9y_9KCHST0M5NaDj4Zg';
 
@@ -9,7 +11,10 @@ export default class Map extends Component {
         super(props);
     }
 
+
     componentDidMount() {
+
+        // Creates map instance
         var map = new mapboxgl.Map({
             container: this.mapContainer,
             style: 'mapbox://styles/mapbox/satellite-streets-v11',
@@ -19,20 +24,21 @@ export default class Map extends Component {
 
         var hoveredStateId = null;
 
-
         map.on('load', function () {
+
+           // Add geojson data to map 
             map.addSource('blenheim_block', {
                 'type': 'geojson',
                 'data': map_data
             })
 
-            // Squares
+            // Code defining how squares behave
             map.addLayer({
                 'id': 'blenheim_block_fill',
                 'type': 'fill',
                 'source': 'blenheim_block',
                 'layout': {},
-                'paint': {
+                'paint': { // Fill color determined by "temperature" feature for each geojson block
                     'fill-color': ["rgb", ["feature-state", "temperature"], ["-", 100, ["feature-state", "temperature"]], ["-", 100, ["feature-state", "temperature"]]],
                     'fill-opacity': ['case', ['boolean', ['feature-state', 'hover'], false], 1, 0.8]
                 }
@@ -50,7 +56,7 @@ export default class Map extends Component {
                 }
             });
 
-            // When the user moves their mouse over the state-fill layer, we'll update the
+            // When the user moves their mouse over the state-fill layer, update the
             // feature state for the feature under the mouse.
             map.on('mousemove', 'blenheim_block_fill', function (e) {
                 if (e.features.length > 0) {
@@ -80,43 +86,25 @@ export default class Map extends Component {
                 hoveredStateId = null;
             });
 
+            // Calls backend api requesting values
+            axios.get(`${backend.value}/api/temperature_sensor/`).then(res => {
 
-            // Temperature array used for dummy data
-            const temps = []
-
-            // For each block, push an empy list and then fill it with randomly changing data
-            for (var block = 0; block < 40; block++) {
-                temps.push([]);
-                temps[block].push(Math.floor((Math.random() * 100) + 1));
-                for (var i = 0; i < 1441; i++) {
-                    var a = Math.floor((Math.random() * 100) + 1)
-                    if (a > 48 && temps[block][i] < 95) {
-                        temps[block].push(temps[block][i] + 1);
-                    } else if (a <= 48 && temps[block][i] > 5) {
-                        temps[block].push(temps[block][i] - 1);
-                    } else {
-                        temps[block].push(temps[block][i]);
-                    }
-
-                }
-            }
-
-            // Set map block temperature property to slider value
-            for (var i = 0; i < 40; i++) {
-                map.setFeatureState({ source: 'blenheim_block', id: i + 1 },
-                    { temperature: temps[i][document.getElementById('slider').value] });
-            }
-
-
-            // Listens for slider change
-            document.getElementById('slider').addEventListener('input', function (e){
-                var hour = parseInt(e.target.value, 10);
-                // Changes each block color to match temperature for that selection
+                // Sets the initial colors of the blocks based on the initial slider value
                 for (var i = 0; i < 40; i++) {
                     map.setFeatureState({ source: 'blenheim_block', id: i + 1 },
-                        { temperature: temps[i][hour] });
+                            { temperature: Math.floor(res.data.filter(x => x.sensor == i+1)[document.getElementById('slider').value].temperature )});
                 }
-            })
+
+                document.getElementById('slider').addEventListener('input', function (e){
+                    // Changes each block color to match temperature for that selection
+                    for (var i = 0; i < 40; i++) {
+                        map.setFeatureState({ source: 'blenheim_block', id: i + 1 },
+                            // Filters data gotten from api call by sensor number. Sets color of block from resulting list based on slider value picked.
+                            { temperature: Math.floor(res.data.filter(x => x.sensor == i+1)[document.getElementById('slider').value].temperature )});
+                    }
+                });
+
+            });
 
         });
 
