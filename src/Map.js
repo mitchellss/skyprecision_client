8eb665bb7e5dc57c91e23e125aa790882411ddef
mitchellss <stephen.mitchell2299@gmail.com>
2022-04-11 20:24:18 -0400

    display data for xchange

--- src/Map.js
+++ src/Map.js
@@ -1,167 +1,190 @@
-import React, { Component } from 'react';
-import mapboxgl from 'mapbox-gl';
-import map_data from './xchange.geojson';
-import axios from 'axios';
-import backend from './globals'
+import React, { Component } from "react";
+import mapboxgl from "mapbox-gl";
+import map_data from "./xchange.geojson";
+import axios from "axios";
+import host from "./globals";
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiaXNhYWNqbWlsbGVyIiwiYSI6ImNrMTZ6NnBqdjFiM3czcHRrb3ZtbTZsajYifQ.3tv9y_9KCHST0M5NaDj4Zg';
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiaXNhYWNqbWlsbGVyIiwiYSI6ImNrMTZ6NnBqdjFiM3czcHRrb3ZtbTZsajYifQ.3tv9y_9KCHST0M5NaDj4Zg";
 
 export default class Map extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            seconds: parseInt(props.startTimeInSeconds, 10) || 0,
-            sensorData: {}
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      seconds: parseInt(props.startTimeInSeconds, 10) || 0,
+      sensorData: {},
+    };
+  }
 
-    componentDidMount() {
+  componentDidMount() {
+    // Creates map instance
+    var map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: "mapbox://styles/mapbox/satellite-streets-v11",
+      center: [-78.86155385515599, 38.43296183843603],
+      zoom: 19,
+    });
 
-        // Creates map instance
-        var map = new mapboxgl.Map({
-            container: this.mapContainer,
-            style: 'mapbox://styles/mapbox/satellite-streets-v11',
-            center: [-78.86155385515599, 38.43296183843603],
-            zoom: 19
-        });
-        
-        this.interval = setInterval((map) => this.tick(map), 2000);
+    var hoveredStateId = null;
 
-        var hoveredStateId = null;
-        const NUM_SENSORS = 40;
-        const MIN_IN_HOUR = 60;
-        const MIN_IN_DAY = 1440;
-        const SEC_IN_MIN = 60;
+    map.on("load", () => {
+      // Add geojson data to map
+      map.addSource("blenheim_block", {
+        type: "geojson",
+        data: map_data,
+      });
 
-        const change_date_display = (time) => {
-            var a = new Date(0);
-            a.setUTCSeconds(time);
+      // Code defining how squares behave
+      map.addLayer({
+        id: "blenheim_block_fill",
+        type: "fill",
+        source: "blenheim_block",
+        layout: {},
+        paint: {
+          // Fill color determined by "temperature" feature for each geojson block
+          "fill-color": [
+            "rgb",
+            ["feature-state", "temperature"],
+            ["-", 100, ["feature-state", "temperature"]],
+            ["-", 100, ["feature-state", "temperature"]],
+          ],
+          // 'fill-color': ['interpolate-hcl', ['linear'], ['get', 'temperature'], 0, 'red', 100, 'blue'],
+          // 'fill-color': ["rgb", 0, 0, 30],
+          // 'fill-color': '#088',
+          "fill-opacity": [
+            "case",
+            ["boolean", ["feature-state", "hover"], false],
+            1,
+            0.8,
+          ],
+        },
+      });
 
-           var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric',  hour:'numeric', minute:'numeric'};
+      // Lines between squares
+      map.addLayer({
+        id: "blenheim_block_border",
+        type: "line",
+        source: "blenheim_block",
+        layout: {},
+        paint: {
+          "line-color": [
+            "rgb",
+            ["feature-state", "temperature"],
+            ["-", 100, ["feature-state", "temperature"]],
+            ["-", 100, ["feature-state", "temperature"]],
+          ],
+          "line-width": 2,
+        },
+      });
 
-            this.props.parent.overlayRef.current.setState({ time_display: `${a.toLocaleDateString("en-US", options)}` });
-
+      // When the user moves their mouse over the state-fill layer, update the
+      // feature state for the feature under the mouse.
+      map.on("mousemove", "blenheim_block_fill", (e) => {
+        if (e.features.length > 0) {
+          if (hoveredStateId) {
+            map.setFeatureState(
+              { source: "blenheim_block", id: hoveredStateId },
+              { hover: false }
+            );
+          }
+          hoveredStateId = e.features[0].id;
+          map.setFeatureState(
+            { source: "blenheim_block", id: hoveredStateId },
+            { hover: true }
+          );
         }
+      });
 
-        map.on('load', function () {
+      // map.on("mousedown", function (e) {
+      //     console.log(e.lngLat)
+      // });
 
-            // Add geojson data to map 
-            map.addSource('blenheim_block', {
-                'type': 'geojson',
-                'data': map_data
-            })
+      // When the mouse leaves the state-fill layer, update the feature state of the
+      // previously hovered feature.
+      map.on("mouseleave", "blenheim_block_fill", () => {
+        if (hoveredStateId) {
+          map.setFeatureState(
+            { source: "blenheim_block", id: hoveredStateId },
+            { hover: false }
+          );
+        }
+        // console.log(hoveredStateId);
+        hoveredStateId = null;
+        // console.log(map.getFeatureState({source: 'blenheim_block', id: "4016624233773201214"}))
+        // console.log(this.state.sensorData)
+        // map.querySourceFeatures("blenheim_block").map(item => {
+        //     console.log(item)
+        // })
+      });
 
-            // Code defining how squares behave
-            map.addLayer({
-                'id': 'blenheim_block_fill',
-                'type': 'fill',
-                'source': 'blenheim_block',
-                'layout': {},
-                'paint': { // Fill color determined by "temperature" feature for each geojson block
-                    'fill-color': ["rgb", ["feature-state", "temperature"], ["-", 100, ["feature-state", "temperature"]], ["-", 100, ["feature-state", "temperature"]]],
-                    'fill-opacity': ['case', ['boolean', ['feature-state', 'hover'], false], 1, 0.8]
-                }
-            });
+      // map.setFeatureState(
+      //     { source: 'blenheim_block', id: hoveredStateId },
+      //     { temperature: 50 }
+      // );
 
-            // Lines between squares
-            map.addLayer({
-                'id': 'blenheim_block_border',
-                'type': 'line',
-                'source': 'blenheim_block',
-                'layout': {},
-                'paint': {
-                    'line-color': ["rgb", ["feature-state", "temperature"], ["-", 100, ["feature-state", "temperature"]], ["-", 100, ["feature-state", "temperature"]]],
-                    'line-width': 2
-                }
-            });
+      var tick = () => {
+        // this.setState(state => ({
+        //     seconds: state.seconds + 1
+        //   }));
 
-            // When the user moves their mouse over the state-fill layer, update the
-            // feature state for the feature under the mouse.
-            map.on('mousemove', 'blenheim_block_fill', function (e) {
-                if (e.features.length > 0) {
-                    if (hoveredStateId) {
+        axios.get("http://localhost:8000").then((res) => {
+          var dict = {};
+          var keys = Object.keys(res.data);
+          var vals = Object.values(res.data);
+
+          var features = map.queryRenderedFeatures({
+            layers: ["blenheim_block_fill"],
+          });
+          features.map(item => {
+              var id = item.properties.sensor_id
+              keys.map((item2, index) => {
+                  if (item2) {
+                      if (id === item2) {
                         map.setFeatureState(
-                            { source: 'blenheim_block', id: hoveredStateId },
-                            { hover: false }
-                        );
-                    }
-                    hoveredStateId = e.features[0].id;
-                    map.setFeatureState(
-                        { source: 'blenheim_block', id: hoveredStateId },
-                        { hover: true }
-                    );
-                }
-            });
-
-            // map.on("mousedown", function (e) {
-            //     console.log(e.lngLat)
-            // });
-
-            // When the mouse leaves the state-fill layer, update the feature state of the
-            // previously hovered feature.
-            map.on('mouseleave', 'blenheim_block_fill', function () {
-                if (hoveredStateId) {
-                    map.setFeatureState(
-                        { source: 'blenheim_block', id: hoveredStateId },
-                        { hover: false }
-                    );
-                }
-                hoveredStateId = null;
-            });
-
+                            { source: "blenheim_block", id: item.id },
+                            { temperature: vals[index] }
+                          );          
+                      }
+                  }
+              })
+          })
+        //   keys.map((item, index) => {
+        //     map.setFeatureState(
+        //       { source: "blenheim_block", id: item },
+        //       { temperature: vals[index] }
+        //     );
+        //     dict[item] = vals[index];
+        //     return null;
+        //   });
+        //   this.setState(() => ({
+        //     sensorData: dict,
+        //   }));
         });
+      };
 
-    }
+      this.interval = setInterval(tick, 2000);
+    });
+  }
 
-    tick(map) {
-        this.setState(state => ({
-          seconds: state.seconds + 1
-        }));
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
 
-        console.log(map)
+  formatTime(secs) {
+    let hours = Math.floor(secs / 3600);
+    let minutes = Math.floor(secs / 60) % 60;
+    let seconds = secs % 60;
+    return [hours, minutes, seconds]
+      .map((v) => ("" + v).padStart(2, "0"))
+      .filter((v, i) => v !== "00" || i > 0)
+      .join(":");
+  }
 
-        axios.get("http://192.168.43.68:42069").then(res => {
-            
-            var dict = {}
-            var keys = Object.keys(res.data)
-            var vals = Object.values(res.data)
-
-            keys.map((item, index) => {
-                dict[item] = vals[index]
-            })
-
-            // console.log(dict)
-            
-
-            this.setState(() => ({
-                sensorData: dict
-            }));
-        })
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.interval);
-    }
-
-
-    formatTime(secs) {
-        let hours   = Math.floor(secs / 3600);
-        let minutes = Math.floor(secs / 60) % 60;
-        let seconds = secs % 60;
-        return [hours, minutes, seconds]
-            .map(v => ('' + v).padStart(2, '0'))
-            .filter((v,i) => v !== '00' || i > 0)
-            .join(':');
-    }
-
-    render() {
-        return (
-            <div>
-                <div ref={el => this.mapContainer = el} className="mapContainer" />
-                {/* <div>
-                    Timer: {this.formatTime(this.state.seconds)}
-                </div>             */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div ref={(el) => (this.mapContainer = el)} className="mapContainer" />
+      </div>
+    );
+  }
 }
--- src/globals.js
+++ src/globals.js
@@ -1,3 +1,5 @@
-export default {
-    value: 'http://192.168.1.125:8000'
+const globalVars = {
+    // host: 'http://192.168.43.68:69420'
+    host: "localhost:8000"
 }
+export default globalVars
